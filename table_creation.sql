9ae890c0b4d8eb0a59064f1c173953c08564ace5
jcybul <46726471+jcybul@users.noreply.github.com>
2020-02-25 15:40:52 +0200

    Update table_creation.sql

--- table_creation.sql
+++ table_creation.sql
@@ -1,1 +1,22 @@
 #
+create table Carousel
+(
+    Barrel_num   int         null,
+    Date_planted date        null,
+    Water_ec     double      null,
+    Seed_type    varchar(30) null,
+    Water_Liters double      null,
+    constraint Carousel_Barrel_num_uindex
+        unique (Barrel_num),
+    check (`Water_ec` = 2.3 or `Water_ec` = 0)
+);
+
+create table Plant_records
+(
+    barrel_num    int          not null,
+    date_recorded datetime     not null,
+    Height        double       null,
+    Comments      varchar(200) null,
+    primary key (barrel_num, date_recorded)
+);
+
